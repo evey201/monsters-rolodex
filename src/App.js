39f42a87c0d3f3fd616e3f1c9d5484831d3c972b
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-03-24 14:46:23 +0100

    added Cardlist component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { CardList } from './components/card-list/card-list.component';
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Dracula',
-          id: 'asc1' 
-        },
-        {
-          name: 'Frankestein',
-          id: 'r2d2'
-        },
-        {
-          name: 'Zombie',
-          id: 'c3po'
-        },
-      ]
+      monsters: []
     };
 
+  }
+
+  componentDidMount() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json())
+    .then(users => this.setState({ monsters: users }));
   }
 
 
   render() {
     return(
       <div className="App">
-        {
-          this.state.monsters.map( monster => (
-            <h1 key={monster.id}>{ monster.name }</h1>
-          ) )
-        }
+        <CardList name='Evey'>
+          {
+            this.state.monsters.map( monster => (
+              <h1 key={monster.id}>{ monster.name }</h1>
+            ) )
+          } 
+        </CardList>
       </div>
     )
   }
