8f13f5b07e4013f0b042f33ce82e29f48bcd02d5
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-03-26 04:22:57 +0100

    Updates

---
+++ precache-manifest.c38884b0eb9964af5db801c45f5ef8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255883b83ccbf1fe0351a79c65a9e332",
+    "url": "/https//evey201.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "44ff12344e7749ff5131",
+    "url": "/https//evey201.github.io/monsters-rolodex/static/css/main.ad2640ad.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/https//evey201.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//evey201.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ff12344e7749ff5131",
+    "url": "/https//evey201.github.io/monsters-rolodex/static/js/main.442d61ec.chunk.js"
+  },
+  {
+    "revision": "1ea069ed8407c19d4ad7",
+    "url": "/https//evey201.github.io/monsters-rolodex/static/js/runtime-main.285aaf00.js"
+  }
+]);
